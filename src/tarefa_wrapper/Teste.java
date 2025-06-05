8e1b2a53f8174a51eaa94e807bfd28b99e7048cb
devdouglasm <devdouglasm@gmail.com>
2025-06-05 15:12:55 -0300

    criando tipo primitivo e wrapper

---
+++ src/tarefa_wrapper/Teste.java
@@ -0,0 +1,13 @@
+package tarefa_wrapper;
+
+public class Teste {
+    public static void main(String[] args) {
+
+        int numero = 10;
+        System.out.println(numero);
+
+        Integer numero1 = numero;
+        System.out.println(numero1);
+
+    }
+}
